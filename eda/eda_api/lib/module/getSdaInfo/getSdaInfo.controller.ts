870d315f1ad8947b941e424ac86a9374535d972b
juanSTIC <juan@sinergiacrm.org>
2024-02-01 09:20:10 +0100

    with db connect

--- eda/eda_api/lib/module/getSdaInfo/getSdaInfo.controller.ts
+++ eda/eda_api/lib/module/getSdaInfo/getSdaInfo.controller.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from "express";
 import * as fs from "fs";
 import * as path from "path";
-import { sinergiaConn } from "../../../config/sinergiacrm.config.js";
+const sinergiaDatabase = require("../../../config/sinergiacrm.config");
+const mariadb = require("mariadb");
 
 export class getSdaInfo {
   public static async getinfo(req: Request, res: Response) {
@@ -9,7 +10,7 @@ export class getSdaInfo {
 
     try {
       const moment = require("moment");
-      
+
       // get lastUpdateModelRun
       const metadataPath = path.join(__dirname, "../../../metadata.json");
       const stats = fs.statSync(metadataPath);
@@ -17,7 +18,12 @@ export class getSdaInfo {
       info["lastUpdateModelRun"] = formattedDate;
 
       // get sinergiaCRMDatabaseName
-      info["sinergiaCRMDatabaseName"] = sinergiaConn.database;
+      info["sinergiaCRMDatabaseName"] =
+        sinergiaDatabase.sinergiaConn.host +
+        ":" +
+        sinergiaDatabase.sinergiaConn.port +
+        "/" +
+        sinergiaDatabase.sinergiaConn.database;
 
       // get sinergiaDaVersion
       const versions = require("../../../../SdaVersion.js");
@@ -26,13 +32,25 @@ export class getSdaInfo {
       // get edaApiVersion from package.json
       const packageJsonPathAPI = path.join(__dirname, "../../../package.json");
       info["edaApiVersion"] = JSON.parse(fs.readFileSync(packageJsonPathAPI, "utf8")).version;
-      
+
       // get edaAppVersion from package.json
       const packageJsonPathAPP = path.join(__dirname, "../../../../eda_app/package.json");
       info["edaAppVersion"] = JSON.parse(fs.readFileSync(packageJsonPathAPP, "utf8")).version;
 
       // get lastSyncDate
-      info["lastSyncDate"] = "GGGGG";
+      let connection: any;
+      connection = await mariadb.createConnection(sinergiaDatabase.sinergiaConn);
+      const rows = await connection.query("SELECT value from sda_def_config WHERE `key` = 'last_rebuild';");
+
+      // async (rows, err1) => {
+      //   if (err1) { console.log("Error getting last_rebuild"); throw err1 }
+      // }
+      if (rows.length > 0) {
+        info["lastSyncDate"] = rows[0].value;
+      } else {
+        info["lastSyncDate"] = "N/D";
+      }
+      connection.end();
 
       // return
       res.json({ info: info });
