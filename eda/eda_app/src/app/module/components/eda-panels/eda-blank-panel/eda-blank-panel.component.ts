32758851e3865b43b9cc599c7b0230a92e21af6c
Asaf Jortilles <asaf@jortilles.com>
2024-02-16 16:32:15 +0100

    2
    corrección, no cambiamos las cadenas vacías, solo los null.

--- eda/eda_app/src/app/module/components/eda-panels/eda-blank-panel/eda-blank-panel.component.ts
+++ eda/eda_app/src/app/module/components/eda-panels/eda-blank-panel/eda-blank-panel.component.ts
@@ -296,7 +296,7 @@ export class EdaBlankPanelComponent implements OnInit {
             try {
                 const response = await QueryUtils.switchAndRun(this, panelContent.query);
                 this.chartLabels = this.chartUtils.uniqueLabels(response[0]);
-                this.chartData = response[1].map(item => item.map(a => a == null || a === '' ? 'null' : a)); // canviem els nulls i cadenes buides per 'null'                
+                this.chartData = response[1].map(item => item.map(a => a == null ? 'null' : a)); // canviem els nulls i cadenes buides per 'null'              
                 this.buildGlobalconfiguration(panelContent);
             } catch (err) {
                 this.alertService.addError(err);
