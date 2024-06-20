5e7b19a1258022d7e3ef8961fbe6c7e98b267156
Juanjo Ortilles <Juanjo Ortilles@sportium.com>
2024-06-20 07:51:51 +0200

    mejor

--- eda/eda_app/src/app/module/components/eda-panels/eda-blank-panel/panel-utils/panel-interaction-utils.ts
+++ eda/eda_app/src/app/module/components/eda-panels/eda-blank-panel/panel-utils/panel-interaction-utils.ts
@@ -289,7 +289,10 @@ export const PanelInteractionUtils = {
                 column.whatif_column = contentColumn.whatif_column || false;
                 column.whatif = contentColumn.whatif || {};
                 column.joins = contentColumn.joins || [];
-                column.column_type = contentColumn.column_type;
+                if(column.column_type!= contentColumn.column_type){
+                  column.old_column_type = column.column_type;
+                  column.column_type = contentColumn.column_type;
+                }
                 PanelInteractionUtils.moveItem(ebp, column);
             } else {
                 if(contentColumn.table_id === idTable) {
