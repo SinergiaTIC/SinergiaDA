dcd40cab8925c001c02ce35851cd0a1f32f5d2bf
Juanjo Ortilles <Juanjo Ortilles@sportium.com>
2024-07-13 08:28:36 +0200

    quitado error fantasma

--- eda/eda_app/src/app/module/components/eda-panels/eda-blank-panel/panel-utils/query-utils.ts
+++ eda/eda_app/src/app/module/components/eda-panels/eda-blank-panel/panel-utils/query-utils.ts
@@ -154,9 +154,6 @@ export const QueryUtils = {
       ebp.index = 1;
       ebp.display_v.saved_panel = true;
     } catch (err) {
-      if (err.status === 555) {
-        err.text =  $localize`:@@noPermitsAlert:No tiene acceso a una o varias de las tablas de la consulta`
-      }
       ebp.alertService.addError(err); 
       ebp.spinnerService.off();
     }
