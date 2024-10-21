73926cb23be877a2a3d3b5f98ef6cd4c2a0941af
juanjo ortilles <juanjo@jortilles.com>
2024-10-21 15:03:47 +0200

    estilo

--- eda/eda_api/lib/module/updateModel/service/push.Model.to.Mongo.ts
+++ eda/eda_api/lib/module/updateModel/service/push.Model.to.Mongo.ts
@@ -20,7 +20,7 @@ export class pushModelToMongo {
                         console.log('El modelo no es el esperado.');
                         res.status(500).json({'status4':'ko'})
                     } else {
-                        console.log('actualizado..... XD');
+                        console.log('insertado.....');
                         const data = await new DataSource(model) ;
                         data.save();
                     }
