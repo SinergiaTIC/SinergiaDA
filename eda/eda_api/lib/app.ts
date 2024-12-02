679bf5c1b2d9c75cc3d2f6b805a65a5298230b8f
juanSTIC <juan@sinergiacrm.org>
2024-12-02 11:17:56 +0100

    set consolelog

--- eda/eda_api/lib/app.ts
+++ eda/eda_api/lib/app.ts
@@ -9,7 +9,8 @@ import errorMiddleware from './middleware/error.middleware';
 import Router from './router';
 
 const path = require('path');
-console.log("NODE_MODULES PATH", path.resolve(process.cwd(), 'node_modules'));
+/* SDA CUSTOM*/ // Show current node_modules folder 
+/* SDA CUSTOM*/ console.log("NODE MODULES PATH",require.resolve('lodash'));
 const database = require('../config/database.config');
 const mongoose = require('mongoose');
 const compression = require('compression');
