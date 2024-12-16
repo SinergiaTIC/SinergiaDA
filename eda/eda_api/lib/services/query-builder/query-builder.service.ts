4a43b4abef0119a0f00f32b19626ae3a310e9176
Juanjo <juanjo@jortilles.com>
2024-12-16 19:47:12 +0100

    Falta revisar la ordenacion

--- eda/eda_api/lib/services/query-builder/query-builder.service.ts
+++ eda/eda_api/lib/services/query-builder/query-builder.service.ts
@@ -336,6 +336,14 @@ export abstract class QueryBuilderService {
                 if(fieldsColumn.ranges.length===0){
                     queryTODO.fields[j]=fieldsColumn;
                 } else {
+                    console.log(this.queryTODO);
+                    console.log('Ronald. Aqui también tienes que gesnerar la ordenacion ascendente o descendente   \
+                        CASE  WHEN sda_stic_payment_commitments.amount < 100 THEN 1 \
+        WHEN sda_stic_payment_commitments.amount >= 100 AND sda_stic_payment_commitments.amount <= 199 THEN 2  \
+        WHEN sda_stic_payment_commitments.amount >= 200 AND sda_stic_payment_commitments.amount <= 999 THEN 3  \
+        WHEN sda_stic_payment_commitments.amount >= 1000 AND sda_stic_payment_commitments.amount <= 9999 THEN 4  \
+        WHEN sda_stic_payment_commitments.amount >= 10000 THEN 5 end  \
+                        ');
 
                     fieldsColumn.computed_column = 'computed';
                     fieldsColumn.column_type = 'text';
@@ -351,7 +359,7 @@ export abstract class QueryBuilderService {
                     for (let i = 0; i < fieldsColumn.ranges.length - 1; i++) {
                         const lower = fieldsColumn.ranges[i];
                         const upper = fieldsColumn.ranges[i + 1] - 1;
-                        SQLexpression += `\tWHEN ${columna} >= ${lower} AND ${columna} <= ${upper} THEN '>=  ${lower} - < ${upper}'\n`;
+                        SQLexpression += `\tWHEN ${columna} >= ${lower} AND ${columna} <= ${upper} THEN ' ${lower} - ${upper}'\n`;
                     }            
         
                     // Último caso: mayor o igual al último valor del rango
