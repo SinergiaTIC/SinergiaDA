e8bb8be404ab47d80ff5a33d0b881c4b64a0ba60
ronaldchavezjortilles <ronald.chavez@jortilles.com>
2025-01-22 19:16:27 +0100

    Ordenamiento de numeric, text y date listo. Planteamiento del ordenamiento del campo que contiene un rango

--- eda/eda_app/src/app/module/components/eda-table/eda-table.component.ts
+++ eda/eda_app/src/app/module/components/eda-table/eda-table.component.ts
@@ -318,6 +318,44 @@ export class EdaTableComponent implements OnInit {
         
         return this.sanitizer.bypassSecurityTrustHtml(str);
 
-      }
+    }
+
+    customSort(event, cols) {
+        
+        const actualField = event.field;
+        const actualCol = cols.find(col => col.field === actualField)
+        
+        console.log('event: ', event)
+        console.log('actualCol: ', actualCol)
+        
+        event.data.sort((data1, data2) => {
+            let value1 = data1[event.field];
+            let value2 = data2[event.field];
+            let result = null;
+
+            if (value1 == null && value2 != null)
+                result = -1;
+            else if (value1 != null && value2 == null)
+                result = 1;
+            else if (value1 == null && value2 == null)
+                result = 0;
+            else if (typeof value1 === 'string' && typeof value2 === 'string') {
+
+                if(actualCol.rangeOption) {
+                    console.log('Debemos hacer la lógica del ordenamiento')
+                } else {
+                    result = value1.localeCompare(value2);
+                }
+
+            }
+            else
+                result = (value1 < value2) ? -1 : (value1 > value2) ? 1 : 0;
+
+            return (event.order * result);
+        });
+
+
+
+    }
 
 }
