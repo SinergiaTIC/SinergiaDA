ee7bdef8c9bc59c98b660493d1d4302cc4c0f534
ronaldchavezjortilles <ronald.chavez@jortilles.com>
2025-04-01 10:48:43 +0200

    Agregado de angular-gridster2, RxJS 7.8.1, limpieza de código y eliminación de carperta de leaflet del node_modules

--- eda/eda_app/src/app/module/components/components.module.ts
+++ eda/eda_app/src/app/module/components/components.module.ts
@@ -6,6 +6,7 @@ import { SharedModule } from '../../shared/shared.module';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { EdaBlankPanelComponent } from './eda-panels/eda-blank-panel/eda-blank-panel.component';
 import { PanelChartComponent} from './eda-panels/eda-blank-panel/panel-charts/panel-chart.component'
+import { GridsterModule } from 'angular-gridster2';
 
 
 // Component
@@ -59,6 +60,7 @@ import { DragDropComponent } from './drag-drop/drag-drop.component';
     imports: [
         CoreModule,
         SharedModule,
+        GridsterModule
     ],
     declarations: [
         EdaBlankPanelComponent,
