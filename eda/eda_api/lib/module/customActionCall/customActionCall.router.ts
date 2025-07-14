d38e8d0f4bf0714bb3da3bc577eb2ee046ca8931
ronaldchavezjortilles <ronald.chavez@jortilles.com>
2025-07-14 12:28:42 +0200

    Cambio de nombre a CustomActionCall

--- eda/eda_api/lib/module/customActionCall/customActionCall.router.ts
+++ eda/eda_api/lib/module/customActionCall/customActionCall.router.ts
@@ -1,10 +1,10 @@
 import * as  express from 'express';
 import { authGuard } from '../../guards/auth-guard';
 import { roleGuard } from '../../guards/role-guard';
-import { UrlFuncionalityController } from './urlFunctionality.controller';
+import { CustomActionCallController } from './customActionCall.controller';
 
 const router = express.Router();
 
-router.post('/check', authGuard, roleGuard, UrlFuncionalityController.checkUrl);
+router.post('/check', authGuard, roleGuard, CustomActionCallController.checkUrl);
 
 export default router;
