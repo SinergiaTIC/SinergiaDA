44874d6c6b5ff7a453936f283bebfa6eebc648c7
ronaldchavezjortilles <ronald.chavez@jortilles.com>
2025-10-07 10:16:28 +0200

    Primera parte de de campos calculados

--- eda/eda_app/src/app/module/pages/data-sources/data-source-detail/calculatedColumn-dialog/calculated-column-dialog.component.ts
+++ eda/eda_app/src/app/module/pages/data-sources/data-source-detail/calculatedColumn-dialog/calculated-column-dialog.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { EdaDialogAbstract, EdaDialog, EdaDialogCloseEvent } from '@eda/shared/components/shared-components.index';
 import { AlertService} from '@eda/services/service.index';
 import { UntypedFormGroup, UntypedFormBuilder, Validators } from '@angular/forms';
+import { SelectItem } from 'primeng/api';
 
 
 @Component({
@@ -15,6 +16,19 @@ export class CalculatedColumnDialogComponent extends EdaDialogAbstract {
   public dialog: EdaDialog;
   public form: UntypedFormGroup;
 
+  // Types
+  public columnTypes: SelectItem[] = [
+      { label: 'text', value: 'text' },
+      { label: 'numeric', value: 'numeric' },
+      { label: 'date', value: 'date' },
+      { label: 'coordinate', value: 'coordinate' }
+  ];
+
+  // Default value
+  public selectedcolumnType = 'numeric';
+  public decimalNumberValue;
+  public sqlExpressionString = '';
+
   constructor(
     private formBuilder: UntypedFormBuilder,
     private alertService: AlertService
@@ -26,11 +40,14 @@ export class CalculatedColumnDialogComponent extends EdaDialogAbstract {
       hide: () => this.onClose(EdaDialogCloseEvent.NONE),
       title: $localize`:@@addCalculatedColTitle:Añadir columna calculada a la tabla `
     });
-    this.dialog.style = { width: '50%', height: '40%', top:"-4em", left:'1em'};
+    this.dialog.style = { width: '55%', height: '60%', top:"-4em", left:'1em'};
 
     this.form = this.formBuilder.group({
       colName: [null, Validators.required],
-      description: [null, Validators.required]
+      description: [null, Validators.required],
+      sqlExpression: [null, Validators.required],
+      typeSelector: [null, Validators.required],
+      decimalNumber: [null, Validators.required],
     });
   }
   onShow(): void {
@@ -60,19 +77,38 @@ export class CalculatedColumnDialogComponent extends EdaDialogAbstract {
         aggregation_type: [{ value: "none", display_name: "No" }],
         column_granted_roles: [],
         column_name: this.form.value.colName,
-        column_type: "numeric",
+        column_type: this.selectedcolumnType,
         description: { default: this.form.value.description, localized: Array(0) },
         display_name: { default: this.form.value.colName, localized: Array(0) },
         row_granted_roles: [],
-        SQLexpression: '',
+        SQLexpression: this.sqlExpressionString,
         computed_column : 'computed',
+        minimumFractionDigits: this.decimalNumberValue,
         visible: true
-
       };
+
+
+      // console.log('decimalNumberValue ===>>> ', this.decimalNumberValue);
+      // console.log('selectedcolumnType ===>>> ', this.selectedcolumnType);
+      // console.log('column ===>>> ', column);
+      // debugger;
+
       this.onClose(EdaDialogCloseEvent.NEW, { column: column, table_name: this.controller.params.table.technical_name });
     }
+    
   }
 
+  onTypeChange(event: any) {
+    const ctrl = this.form.get('decimalNumber');
+    if (!ctrl) return;
+
+    if (event.value === 'numeric') {
+      ctrl.enable();
+    } else {
+      ctrl.reset();
+      ctrl.disable();
+    }
+}
 
 
 }
