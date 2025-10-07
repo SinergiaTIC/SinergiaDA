5a08e31b83a0a03ebf0bb04c2c7d2394e90ab739
ronaldchavezjortilles <ronald.chavez@jortilles.com>
2025-10-07 17:02:58 +0200

    dialog creado y validación del formulario

--- eda/eda_app/src/app/module/pages/data-sources/data-source-detail/calculated-column-edit-dialog/calculated-column-edit-dialog.component.ts
+++ eda/eda_app/src/app/module/pages/data-sources/data-source-detail/calculated-column-edit-dialog/calculated-column-edit-dialog.component.ts
@@ -1,4 +1,7 @@
 import { Component, EventEmitter, OnInit, Output, Input } from '@angular/core';
+import { UntypedFormGroup, UntypedFormBuilder, Validators } from '@angular/forms';
+import { SelectItem } from 'primeng/api';
+import { AlertService } from '@eda/services/service.index';
 
 @Component({
   selector: 'app-calculated-column-edit-dialog',
@@ -11,17 +14,86 @@ export class CalculatedColumnEditDialogComponent implements OnInit {
   @Output() close: EventEmitter<any> = new EventEmitter<any>();
   
   public display: boolean = false;
+  public form: UntypedFormGroup;
 
-  constructor() { }
+  // Types
+  public columnTypes: SelectItem[] = [
+      { label: 'text', value: 'text' },
+      { label: 'numeric', value: 'numeric' },
+      { label: 'date', value: 'date' },
+      { label: 'coordinate', value: 'coordinate' }
+  ];
 
-  ngOnInit(): void {
+  // Default value
+  public name = '';
+  public description = '';
+  public sqlExpressionString = '';  
+  public selectedcolumnType = '';
+  public decimalNumberValue;
 
-    console.log('Columna recibida:', this.column);
+  constructor(
+    private formBuilder: UntypedFormBuilder,
+      private alertService: AlertService,
+) {
+
+    this.form = this.formBuilder.group({
+      colName: [null, Validators.required],
+      description: [null, Validators.required],
+      sqlExpression: [null, Validators.required],
+      typeSelector: [null, Validators.required],
+      decimalNumber: [null, Validators.required],
+    });
 
   }
 
+  ngOnInit(): void {
+
+    const column = this.column;
+    this.initForm(column);
+
+    // this.selectedcolumnType = this.column.
+  }
+
+  initForm(column) {
+
+    console.log('column:', column);
+
+    this.name = column.name;
+    this.description = column.description;
+    this.sqlExpressionString = column.SQLexpression;  
+    this.selectedcolumnType = column.column_type;
+    this.decimalNumberValue = column.minimumFractionDigits;
+
+
+    console.log('name: ', this.name)
+    console.log('description: ', this.description)
+    console.log('sqlExpressionString: ', this.sqlExpressionString)
+    console.log('selectedcolumnType: ', this.selectedcolumnType)
+    console.log('decimalNumberValue: ', this.decimalNumberValue)
+
+    if(this.selectedcolumnType !== 'numeric') {
+      this.decimalNumberValue = null;
+      const ctrl = this.form.get('decimalNumber');
+      ctrl.reset();
+      ctrl.disable();    
+    }
+    
+}
+
   onApplyCalculatedColumn(){
-    console.log('onApplyCalculatedColumn')
+    console.log('Aplicando los cambios aqui::::::::::::::::  ');
+
+    if(this.form.invalid) {
+      return this.alertService.addError($localize`:@@formDialogCalculatedColumn:Recuerde rellenar todos los campos`);
+    } else {
+      console.log('FORMULARIO CORRECTO....')
+    }
+
+    // console.log('name: ', this.name)
+    // console.log('description: ', this.description)
+    // console.log('sqlExpressionString: ', this.sqlExpressionString)
+    // console.log('selectedcolumnType: ', this.selectedcolumnType)
+    // console.log('decimalNumberValue: ', this.decimalNumberValue)
 
   }
 
@@ -29,5 +101,17 @@ export class CalculatedColumnEditDialogComponent implements OnInit {
     this.close.emit();
   }
 
+
+  onTypeChange(event: any) {
+
+    const ctrl = this.form.get('decimalNumber');
+    if (!ctrl) return;
+    if (event.value === 'numeric') {
+      ctrl.enable();
+    } else {
+      ctrl.reset();
+      ctrl.disable();
+    }
+  }
 
 }
