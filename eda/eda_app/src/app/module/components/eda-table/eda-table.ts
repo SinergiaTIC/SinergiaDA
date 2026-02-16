84894c98a64741a13aa2e2f5fd966e5c4316d560
ronaldchavezjortilles <ronald.chavez@jortilles.com>
2026-02-16 16:37:33 +0100

    Agregado de rangeOption para las tablas cruzadas

--- eda/eda_app/src/app/module/components/eda-table/eda-table.ts
+++ eda/eda_app/src/app/module/components/eda-table/eda-table.ts
@@ -969,7 +969,7 @@ export class EdaTable {
 
         const tableColumns = [];
         params.mainCols.forEach(element => {
-            tableColumns.push(new EdaColumnText({ header: element['header'], field: element['field'] }))
+            tableColumns.push(new EdaColumnText({ header: element['header'], field: element['field'], rangeOption: element['rangeOption'] }))
         })
         newColNames.forEach(col => {
             tableColumns.push(new EdaColumnNumber({ header: col, field: col }));
