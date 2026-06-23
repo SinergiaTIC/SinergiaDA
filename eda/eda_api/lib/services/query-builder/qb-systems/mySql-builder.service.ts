7a7c7402380b3ec93ae6a6ef455a425f33bf433d
Paula <paula@sinergiacrm.org>
2026-06-23 14:30:59 +0200

    Fix error

--- eda/eda_api/lib/services/query-builder/qb-systems/mySql-builder.service.ts
+++ eda/eda_api/lib/services/query-builder/qb-systems/mySql-builder.service.ts
@@ -362,7 +362,7 @@ export class MySqlBuilderService extends QueryBuilderService {
     sortedFilters.sort((a: any, b: any) => a.y - b.y); 
 
     // Calculating global filters and they are empty.
-    const nullSortedFilters  =  sortedFilters.filter((f: any) => ((f.isGlobal===true) && (f.filter_elements[0].value1.length === 0)));
+    const nullSortedFilters  =  sortedFilters.filter((f: any) => ((f.isGlobal===true) && (f.filter_elements?.length > 0 && f.filter_elements[0]?.value1?.length === 0)));
 
     // If we have empty values in the filters we define a new sortedFilters
     if(nullSortedFilters.length !==0){
@@ -382,7 +382,7 @@ export class MySqlBuilderService extends QueryBuilderService {
       }  )
   
       // Order in the y axis
-      const newSortedFilters = sortedFilters.filter((f: any) => !((f.isGlobal===true) && (f.filter_elements[0].value1.length === 0)));
+      const newSortedFilters = sortedFilters.filter((f: any) => !((f.isGlobal===true) && (f.filter_elements?.length > 0 && f.filter_elements[0]?.value1?.length === 0)));
       newSortedFilters.forEach( (f,i) => f.y=i );
 
       sortedFilters = _.cloneDeep(newSortedFilters);
@@ -390,7 +390,7 @@ export class MySqlBuilderService extends QueryBuilderService {
 
     // If we have a global filter with only one empty value selected
     filters.forEach(filter => {
-      if(filter.isGlobal && (filter.filter_type === 'null_or_empty') && (filter.filter_elements[0].value1[0]==='emptyString')) {
+      if(filter.isGlobal && (filter.filter_type === 'null_or_empty') && (filter.filter_elements?.length > 0 && filter.filter_elements[0]?.value1?.[0]==='emptyString')) {
         const selectedFilter = sortedFilters.find(sf => sf.filter_id === filter.filter_id);
 
         if(selectedFilter) {
@@ -564,7 +564,7 @@ export class MySqlBuilderService extends QueryBuilderService {
         resultado = `${['null_or_empty', 'not_null_nor_empty'].includes(filter_type) || (filter_type==='in' && sqlOptional !== undefined) ? ' (' : ''} ${sqlOptional !== undefined ? sqlOptional : ''} (${SQLexpression}) ${filter_type_value}${filter_elements_value}`;
       } else {
         /* SDA CUSTOM */ const rawColRef = `\`${validador ? valueListSource.target_table : filter_table}\`.\`${valueListFilterColumn}\``;
-        /* SDA CUSTOM */ const dateFilterColRef = (filter_column_type === 'date' && !['between', 'not_between'].includes(filter_type) && !(dynamicValue && (filter_type === 'in' || filter_type === 'not_in')))
+        /* SDA CUSTOM */ const dateFilterColRef = (filter_column_type === 'date' && !['between', 'not_between'].includes(filter_type) && !['not_null', 'not_null_nor_empty', 'null_or_empty'].includes(filter_type) && !(dynamicValue && (filter_type === 'in' || filter_type === 'not_in')))
         /* SDA CUSTOM */   ? `DATE_FORMAT(${rawColRef}, '%Y-%m-%d')`
         /* SDA CUSTOM */   : rawColRef;
         /* SDA CUSTOM */ resultado = `${['null_or_empty', 'not_null_nor_empty'].includes(filter_type) || (filter_type==='in' && sqlOptional !== undefined) ? ' (' : ''} ${sqlOptional !== undefined ? sqlOptional : ''} ${dateFilterColRef} ${filter_type_value}${filter_elements_value}`;
