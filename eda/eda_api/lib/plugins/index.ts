094444c68e71ae43d2fca9a56d8791346e8cdf3a
ronaldchavezjortilles <ronald.chavez@jortilles.com>
2026-06-27 19:10:30 +0200

    UpdateModel terminado y probado

--- eda/eda_api/lib/plugins/index.ts
+++ eda/eda_api/lib/plugins/index.ts
@@ -2,9 +2,11 @@ import { PluginRegistry } from './plugin-registry';
 import { GoogleAnalyticsPlugin } from './google-analytics';
 import { OdooPlugin } from './odoo';
 import { HoldedPlugin } from './holded';
+import { UpdateModelPlugin } from './updateModel';
 
 PluginRegistry.register(GoogleAnalyticsPlugin);
 PluginRegistry.register(OdooPlugin);
 PluginRegistry.register(HoldedPlugin);
+PluginRegistry.register(UpdateModelPlugin);
 
 export { PluginRegistry };
