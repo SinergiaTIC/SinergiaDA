fbc6f8a2bd1969bee728fb5aa36176c79875cc82
Juanjo <juanjo@jortilles.com>
2026-07-31 16:10:50 +0200

    permisos en modo SQL

--- eda/eda_api/config/eda_api_config.js
+++ eda/eda_api/config/eda_api_config.js
@@ -17,6 +17,6 @@ module.exports = {
     error_log_file: "XXXXXXXXXXXXXXXXXX", // server error log
     custom_behaviour:{
       ALLOW_NON_ADMIN_MANAGE_PUBLIC_REPORTS: false, // true -> Non-admin users can manage "open" dashboards.
-      USE_FLAT_PERMISSIONS: false // true -> QueryBuilderService.builder() getPermissions (EDA). false -> with getTreePermissions (SDA).
+      USE_RECURSIVE_PERMISSIONS: false // true -> QueryBuilderService.builder() getPermissions (EDA). false -> with getTreePermissions (SDA).
     }
   }
