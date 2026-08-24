40fc577bd44e0f94def32f78ace16c37b90b9169
ronaldchavezjortilles <ronald.chavez@jortilles.com>
2026-08-24 12:25:37 +0200

    Eliminacion de plugins

--- eda/eda_api/lib/plugins/updateModel/index.ts
+++
@@ -1,9 +0,0 @@
-import { IFeaturePlugin } from '../plugin.interface';
-import UpdateModelRouter from './updateModel.router';
-
-export const UpdateModelPlugin: IFeaturePlugin = {
-    kind: 'feature',
-    type: 'updatemodel',
-    router: UpdateModelRouter,
-    routerPath: '/updatemodel',
-};
