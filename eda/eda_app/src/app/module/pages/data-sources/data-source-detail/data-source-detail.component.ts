57f0bf65615b9218a381a2cda040a16124bdb00f
ronaldchavezjortilles <ronald.chavez@jortilles.com>
2026-09-16 21:50:06 +0200

    3 botones del model generico de fuentes de datos

--- eda/eda_app/src/app/module/pages/data-sources/data-source-detail/data-source-detail.component.ts
+++ eda/eda_app/src/app/module/pages/data-sources/data-source-detail/data-source-detail.component.ts
@@ -6,7 +6,7 @@ import { MenuItem, SelectItem, TreeNode } from 'primeng/api';
 import { AlertService, DataSourceService, QueryParams, QueryBuilderService, SpinnerService } from '@eda/services/service.index';
 import { EditTablePanel, EditColumnPanel, EditModelPanel, ValueListSource, Relation } from '@eda/models/data-source-model/data-source-models';
 import { EdaDialogController, EdaDialogCloseEvent, EdaContextMenu, EdaContextMenuItem } from '@eda/shared/components/shared-components.index';
-import { AGG_TYPES, PROTECTED_MODEL_AI_CONTROL_ENABLED, PROTECTED_MODEL_SSL_CONNECTION_SWITCH_BUTTON_ENABLED } from '@eda/configs/customizable/customizable_default';
+import { AGG_TYPES, PROTECTED_MODEL_AI_CONTROL_ENABLED, PROTECTED_MODEL_SSL_CONNECTION_SWITCH_BUTTON_ENABLED, PROTECTED_MODEL_ADD_VIEW_BUTTON_ENABLED, PROTECTED_MODEL_ADD_TABLE_FROM_CSV_BUTTON_ENABLED, PROTECTED_MODEL_ADD_TAG_BUTTON_ENABLED } from '@eda/configs/customizable/customizable_default';
 import { EdaColumnFunction } from '@eda/components/eda-table/eda-columns/eda-column-function';
 import * as _ from 'lodash';
 import { EdaColumnEditable } from '@eda/components/eda-table/eda-columns/eda-column-editable';
@@ -77,6 +77,9 @@ export class DataSourceDetailComponent implements OnInit, OnDestroy {
 
     public aiControlEnabled: boolean = PROTECTED_MODEL_AI_CONTROL_ENABLED;
     public sslConnectionSwitchEnabled: boolean = PROTECTED_MODEL_SSL_CONNECTION_SWITCH_BUTTON_ENABLED;
+    public addViewButtonEnabled: boolean = PROTECTED_MODEL_ADD_VIEW_BUTTON_ENABLED;
+    public addTableFromCsvButtonEnabled: boolean = PROTECTED_MODEL_ADD_TABLE_FROM_CSV_BUTTON_ENABLED;
+    public addTagButtonEnabled: boolean = PROTECTED_MODEL_ADD_TAG_BUTTON_ENABLED;
 
     public form: UntypedFormGroup;
     public permissionsColumn: EdaTable;
