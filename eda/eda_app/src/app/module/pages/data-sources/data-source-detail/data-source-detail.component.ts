ffe676a58ee46b9bb47e61b1faebfcce1ad36e4b
ronaldchavezjortilles <ronald.chavez@jortilles.com>
2026-09-16 21:07:37 +0200

    PROTECTED_MODEL_AI_CONTROL_ENABLED & PROTECTED_MODEL_SSL_CONNECTION_SWITCH_BUTTON_ENABLED

--- eda/eda_app/src/app/module/pages/data-sources/data-source-detail/data-source-detail.component.ts
+++ eda/eda_app/src/app/module/pages/data-sources/data-source-detail/data-source-detail.component.ts
@@ -6,7 +6,7 @@ import { MenuItem, SelectItem, TreeNode } from 'primeng/api';
 import { AlertService, DataSourceService, QueryParams, QueryBuilderService, SpinnerService } from '@eda/services/service.index';
 import { EditTablePanel, EditColumnPanel, EditModelPanel, ValueListSource, Relation } from '@eda/models/data-source-model/data-source-models';
 import { EdaDialogController, EdaDialogCloseEvent, EdaContextMenu, EdaContextMenuItem } from '@eda/shared/components/shared-components.index';
-import { AGG_TYPES } from '@eda/configs/customizable/customizable_default';
+import { AGG_TYPES, PROTECTED_MODEL_AI_CONTROL_ENABLED, PROTECTED_MODEL_SSL_CONNECTION_SWITCH_BUTTON_ENABLED } from '@eda/configs/customizable/customizable_default';
 import { EdaColumnFunction } from '@eda/components/eda-table/eda-columns/eda-column-function';
 import * as _ from 'lodash';
 import { EdaColumnEditable } from '@eda/components/eda-table/eda-columns/eda-column-editable';
@@ -74,6 +74,9 @@ const STANDALONE_COMPONENTS = [
 })
 export class DataSourceDetailComponent implements OnInit, OnDestroy {
     @Output() onTableCreated: EventEmitter<any> = new EventEmitter();
+
+    public aiControlEnabled: boolean = PROTECTED_MODEL_AI_CONTROL_ENABLED;
+    public sslConnectionSwitchEnabled: boolean = PROTECTED_MODEL_SSL_CONNECTION_SWITCH_BUTTON_ENABLED;
 
     public form: UntypedFormGroup;
     public permissionsColumn: EdaTable;
